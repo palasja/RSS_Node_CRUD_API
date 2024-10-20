e59c649d9c81b4c786d341ce2bd3b28a206ffbda
Yakubenka Ivan <palasja@gmail.com>
2024-10-20 19:52:11 +0300

    feat: dd cluster with test and bd

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,39 @@
 import { User } from "./types";
 import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
+//:{userArr: User[], getAll: () =>  User[]}
+const userArr: User[]= [
+    {
+      id: uuidv4(),
+      username: 'palasja',
+      age: 18,
+      hobbies: ['coding', 'anime'],
+    },
+    {
+      id: uuidv4(),
+      username: 'Ya',
+      age: 33,
+      hobbies: ['qwe', 'asd'],
+    }
+  ]
+// let bd = {
+ 
+//   // userArr:[
+//   //   {
+//   //     id: uuidv4(),
+//   //     username: 'palasja',
+//   //     age: 18,
+//   //     hobbies: ['coding', 'anime'],
+//   //   },
+//   //   {
+//   //     id: uuidv4(),
+//   //     username: 'Ya',
+//   //     age: 33,
+//   //     hobbies: ['qwe', 'asd'],
+//   //   }
+//   // ],
+//   getAll: (): User[] => {return userArr},
+//   add: (u: User) => {userArr.push(u)}
+// }
 
-let userArr: User[] = [
-  {
-    id: uuidv4(),
-    username: 'palasja',
-    age: 18,
-    hobbies: ['coding', 'anime'],
-  },
-  {
-    id: uuidv4(),
-    username: 'Ya',
-    age: 33,
-    hobbies: ['qwe', 'asd'],
-  }
-];
 
 export default userArr
